02f62af432cac04efc94616470fc8282c616adf6
Avishka-Shamendra <avishka199849@gmail.com>
2021-08-28 15:07:56 +0530

    added test to create lab managers and admins

--- lab_manager_user/urls.py
+++ lab_manager_user/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import LabManagerRegisterAPIView
 
 urlpatterns = [
-    path('register/',LabManagerRegisterAPIView.as_view(),name='admin-register'),
+    path('register/',LabManagerRegisterAPIView.as_view(),name='lab-manager-register'),
 ]
