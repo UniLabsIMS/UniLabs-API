f4ca25b65a01ccf80ca3447a347975fb5dac6602
Avishka-Shamendra <avishka199849@gmail.com>
2021-09-17 13:35:32 +0530

    added atomic to create method of user

--- lab_manager_user/models.py
+++ lab_manager_user/models.py
@@ -7,19 +7,20 @@ from department.models import Department
 from django.db import models
 from custom_user.models import User
 from custom_user.models import Role
+from django.db import transaction
 
 # Admin manager to create lab managers
 class LabManagerUserManager(BaseUserManager):
+    @transaction.atomic
     def create_lab_manager(self, email, lab, department):
         lab_manager=self.model(email=self.normalize_email(email),lab=lab, department=department, role= Role.LAB_MANAGER)
         password = DefaultPasswords.DEFAULT_DEBUG_LAB_MANAGER_PASSWORD if (config('DEBUG','True')=='True') else self.make_random_password()
-        print('Password>>>>>>>>>>>>>>>'+' '+password) #TODO: Remove this when email functionality done
         lab_manager.set_password(password)
+        lab_manager.save()
         try:
             Email.send_new_registration_email(email,Role.LAB_MANAGER,password)
         except:
             raise Exception('Error sending new registration email')
-        lab_manager.save()
         return lab_manager
 
 #Lab Manager model which extends User model
