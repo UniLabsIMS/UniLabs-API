8503a7bcbc6ec79a78173d8a1d3431c4de475cab
Avishka-Shamendra <avishka199849@gmail.com>
2021-10-14 19:30:44 +0530

    lab report end point review

--- lab/serializers.py
+++ lab/serializers.py
@@ -1,5 +1,9 @@
 from django.db import transaction
 from rest_framework.exceptions import ValidationError
+from display_item.models import DisplayItem
+from item_category.models import ItemCategory
+from lab_assistant_user.models import LabAssistant
+from lab_manager_user.models import LabManager
 from lecturer_user.models import LabLecturer, Lecturer
 from department.serializers import DepartmentReadSerializer
 from .models import Lab
@@ -73,11 +77,15 @@ class LabAssignLecturerSerializer(serializers.ModelSerializer):
 # lab report
 class LabReportReadSerializer(serializers.Serializer):
     lab_id = serializers.CharField(write_only=True)
+    total_category_count=serializers.SerializerMethodField(read_only=True)
+    total_display_item_count=serializers.SerializerMethodField(read_only=True)
     total_item_count = serializers.SerializerMethodField(read_only=True)
-    currently_borrowed_items_count=serializers.SerializerMethodField(read_only=True)
-    percentage_of_borrowed_items_currently=serializers.SerializerMethodField(read_only=True)
+    available_item_count = serializers.SerializerMethodField(read_only=True)
+    borrowed_items_count=serializers.SerializerMethodField(read_only=True)
+    temp_borrowed_items_count=serializers.SerializerMethodField(read_only=True)
     damaged_items_count=serializers.SerializerMethodField(read_only=True)
-    percentage_of_damaged_items=serializers.SerializerMethodField(read_only=True)
+    lab_manager_count=serializers.SerializerMethodField(read_only=True)
+    lab_assistant_count = serializers.SerializerMethodField(read_only=True)
 
     def validate(self, data):
         lab_id=data.get('lab_id')
@@ -88,22 +96,41 @@ class LabReportReadSerializer(serializers.Serializer):
 
         return data
     
+    def get_total_category_count(self,validated_data):
+        self.lab_id=validated_data.get('lab_id')
+        total_category_count = ItemCategory.objects.filter(lab_id=self.lab_id).count()
+        self.total_category_count=total_category_count
+        return total_category_count
+
+    def get_total_display_item_count(self,validated_data):
+        self.lab_id=validated_data.get('lab_id')
+        total_display_item_count = DisplayItem.objects.filter(lab_id=self.lab_id).count()
+        self.total_display_item_count=total_display_item_count
+        return total_display_item_count
+
     def get_total_item_count(self,validated_data):
         self.lab_id=validated_data.get('lab_id')
         total_item_count = Item.objects.filter(lab_id=self.lab_id).count()
         self.total_item_count=total_item_count
         return total_item_count
-    
-    def get_currently_borrowed_items_count(self,validated_data):
+
+    def get_available_item_count(self,validated_data):
         self.lab_id=validated_data.get('lab_id')
-        currently_borrowed_items_count = Item.objects.filter(lab_id=self.lab_id,state=State.BORROWED).count()
-        self.currently_borrowed_items_count=currently_borrowed_items_count
-        return currently_borrowed_items_count
+        available_item_count = Item.objects.filter(lab_id=self.lab_id,state=State.AVAILABLE).count()
+        self.available_item_count=available_item_count
+        return available_item_count
     
-    def get_percentage_of_borrowed_items_currently(self,validated_data):
-        percentage_of_borrowed_items_currently = round(self.currently_borrowed_items_count/self.total_item_count,2)*100
-        self.percentage_of_borrowed_items_currently=percentage_of_borrowed_items_currently
-        return percentage_of_borrowed_items_currently
+    def get_borrowed_items_count(self,validated_data):
+        self.lab_id=validated_data.get('lab_id')
+        borrowed_items_count = Item.objects.filter(lab_id=self.lab_id,state=State.BORROWED).count()
+        self.borrowed_items_count=borrowed_items_count
+        return borrowed_items_count
+    
+    def get_temp_borrowed_items_count(self,validated_data):
+        self.lab_id=validated_data.get('lab_id')
+        temp_borrowed_items_count = Item.objects.filter(lab_id=self.lab_id,state=State.TEMP_BORROWED).count()
+        self.temp_borrowed_items_count=temp_borrowed_items_count
+        return temp_borrowed_items_count
     
     def get_damaged_items_count(self,validated_data):
         self.lab_id=validated_data.get('lab_id')
@@ -111,10 +138,17 @@ class LabReportReadSerializer(serializers.Serializer):
         self.damaged_items_count=damaged_items_count
         return damaged_items_count
     
-    def get_percentage_of_damaged_items(self,validated_data):
-        percentage_of_damaged_items = round(self.damaged_items_count/self.total_item_count,2)*100
-        self.percentage_of_damaged_items=percentage_of_damaged_items
-        return percentage_of_damaged_items
+    def get_lab_manager_count(self,validated_data):
+        self.lab_id=validated_data.get('lab_id')
+        lab_manager_count = LabManager.objects.filter(lab_id=self.lab_id).count()
+        self.lab_manager_count=lab_manager_count
+        return lab_manager_count
+    
+    def get_lab_assistant_count(self,validated_data):
+        self.lab_id=validated_data.get('lab_id')
+        lab_assistant_count = LabAssistant.objects.filter(lab_id=self.lab_id).count()
+        self.lab_assistant_count=lab_assistant_count
+        return lab_assistant_count
     
 
             
