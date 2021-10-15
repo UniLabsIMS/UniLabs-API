bd0ccd94c2b03bef47fdb4e514ecb5f3f6efda58
Avishka-Shamendra <avishka199849@gmail.com>
2021-10-15 11:35:24 +0530

    added endpoint to get lecturers of lab

--- lecturer_user/urls.py
+++ lecturer_user/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import LecturerRegisterAPIView, AllLecturersAPIView
+from .views import LabLecturersAPIView, LecturerRegisterAPIView, AllLecturersAPIView
 
 urlpatterns = [
     path('register/',LecturerRegisterAPIView.as_view(),name='lecturer-register'),
+    path('of-lab/<str:lab_id>',LabLecturersAPIView.as_view(),name='lecturers-of-lab'),
     path('',AllLecturersAPIView.as_view(),name='all-lecturers'),
 ]
--- lecturer_user/views.py
+++ lecturer_user/views.py
@@ -1,9 +1,10 @@
+from rest_framework.exceptions import ValidationError
 from custom_user.permissions import IsAdmin
 from rest_framework import generics,status
 from rest_framework.response import Response
-from .serializers import LecturerRegisterSerializer, LecturerReadSerializer
+from .serializers import LecturerRegisterSerializer, LecturerReadSerializer, LecturerSummarizedReadSerializer
 from rest_framework.permissions import IsAuthenticated
-from .models import Lecturer
+from .models import LabLecturer, Lecturer
 from rest_framework.permissions import IsAuthenticated
 
 # Lecturer Register View
@@ -19,8 +20,27 @@ class LecturerRegisterAPIView(generics.GenericAPIView):
         lecturer_data = serializer.data        
         return Response(lecturer_data,status=status.HTTP_201_CREATED)
 
-# GET request to get all lab Lecturers in the system as a list
+# GET request to get all Lecturers in the system as a list
 class AllLecturersAPIView(generics.ListAPIView):
     serializer_class=LecturerReadSerializer
     queryset=Lecturer.objects.all()
     permission_classes=(IsAuthenticated,IsAdmin)
+
+
+# GET request to get all Lecturers of a lab in the system as a list
+class LabLecturersAPIView(generics.ListAPIView):
+    serializer_class=LecturerSummarizedReadSerializer
+    queryset=Lecturer.objects.all()
+    permission_classes=(IsAuthenticated,)
+    
+    def get_queryset(self):
+        try:
+            lab_lecturers=LabLecturer.objects.filter(lab=self.kwargs.get('lab_id',None)) # get only lab lecturers
+            lecturers=[] 
+            for lab_lecturer in lab_lecturers:
+                lecturers.append(lab_lecturer.lecturer.id)
+            return self.queryset.filter(id__in=lecturers)
+
+        except:
+            raise ValidationError('Provided lab id invalid')
+        
