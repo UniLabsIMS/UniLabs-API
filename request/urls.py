0c5c2d19b37be4c7db51830ae7a731aa750daeb1
Shanmugabavan <shanmugabavan25621@gmail.com>
2021-10-26 12:41:57 +0530

    request urls tests added

--- request/urls.py
+++ request/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ApprovedRequestItemsListFromLabForStudentAPIView, ClearApprovedRequestItemsFromLabForStudentAPIView, RequestCreateAPIView,RequestListByStudentView,RequestListByLecturerView, RequestRetrieveAPIView,RequestsListByLabAPIView,RequestUpdateSerializer, StudentCheckForActiveRequestInLabAPIView
+from .views import ApprovedRequestItemsListFromLabForStudentAPIView, ClearApprovedRequestItemsFromLabForStudentAPIView, RequestCreateAPIView,RequestListByStudentView,RequestListByLecturerView, RequestRetrieveAPIView,RequestsListByLabAPIView,RequestUpdateAPIView, StudentCheckForActiveRequestInLabAPIView
 
 urlpatterns=[
     path('create/',RequestCreateAPIView.as_view(),name='new-request'),
@@ -7,7 +7,7 @@ urlpatterns=[
     path('student-requests/',RequestListByStudentView.as_view(),name='student-requests'),
     path('lecturer-requests/',RequestListByLecturerView.as_view(),name='lecturer-requests'),
     path('of-requests/<str:lab_id>', RequestsListByLabAPIView.as_view(), name='requests-of-a-lab'),
-    path('approve-or-decline/<str:id>',RequestUpdateSerializer.as_view(),name='approve-or-decline'),
+    path('approve-or-decline/<str:id>',RequestUpdateAPIView.as_view(),name='approve-or-decline'),
     path('approved-request-items/<str:lab_id>/<str:student_id>', ApprovedRequestItemsListFromLabForStudentAPIView.as_view(), name='approved-display-items-in-lab-for-student'),
     path('clear-approved-request-items/', ClearApprovedRequestItemsFromLabForStudentAPIView.as_view(), name='clear-approved-display-items'), 
     path('check-student-active-request/<str:lab_id>', StudentCheckForActiveRequestInLabAPIView.as_view(), name='check-for-student-active-request-in-lab'), 
